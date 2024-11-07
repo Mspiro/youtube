76ce04ac0a145e11c5ae4250f29db4feaee9554c
Meeninath Dhobale <meeninath.dhobale@qed42.com>
2024-11-07 18:25:09 +0530

    Video play page completed using API Data with channel details comment details and other stuff.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 import menu_icon from "../../assets/menu.png";
 import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search.png";
@@ -8,12 +9,19 @@ import more_icon from "../../assets/more.png";
 import notification_icon from "../../assets/notification.png";
 import profile_icon from "../../assets/jack.png";
 
-const Navbar = ({setSidebar}) => {
+const Navbar = ({ setSidebar }) => {
   return (
     <nav className="flex-div">
       <div className="nav-left flex-div">
-        <img className="menu-icon" onClick={()=>setSidebar(prev=>prev===false?true:false)} src={menu_icon} alt="" />
-        <img className="logo" src={logo} alt="" />
+        <img
+          className="menu-icon"
+          onClick={() => setSidebar((prev) => (prev === false ? true : false))}
+          src={menu_icon}
+          alt=""
+        />
+        <Link to="/">
+          <img className="logo" src={logo} alt="" />
+        </Link>
       </div>
 
       <div className="nav-middle flex-div">
--- src/data.js
+++ src/data.js
@@ -2,9 +2,9 @@ export const API_KEY = "AIzaSyCQetDCWUNsYJlF8fmPIotZ9LQigq-DcUI";
 
 export const value_converter = (value) => {
   if (value >= 100000) {
-    return Math.floor(value / 1000000) + "M";
+    return Math.floor(value / 1000000) + "M ";
   } else if (value >= 1000) {
-    return Math.floor(value / 1000) + "K";
+    return Math.floor(value / 1000) + "K ";
   } else {
     return value;
   }
