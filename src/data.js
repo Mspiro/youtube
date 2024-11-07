bf91c105ac074db1d4e762d94eb213f685729f69
Meeninath Dhobale <meeninath.dhobale@qed42.com>
2024-11-07 19:17:11 +0530

    code sanitization

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const value_converter = (value) => {
   if (value >= 100000) {
-    return Math.floor(value / 1000000) + "M ";
+    return Math.floor(value / 100000) + "M ";
   } else if (value >= 1000) {
     return Math.floor(value / 1000) + "K ";
   } else {
